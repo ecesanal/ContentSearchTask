438e3ceddb4dcc05cbee8d88185ae30bc9b7cf82
ecesanal <elifecesanal4@gmail.com>
2025-10-24 17:11:06 +0300

    php task all fix last

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -20,16 +20,16 @@ class SearchController extends Controller
         $langId = $request->input('lang');
         $languages = Language::all();
         $results = collect();
+        
+        $results = Content::where(function ($q) use ($query) {
+        $q->where('title', 'LIKE', "%{$query}%")
+          ->orWhere('content', 'LIKE', "%{$query}%");
+    })
+    ->orderByRaw("CASE WHEN language_id = ? THEN 0 ELSE 1 END", [$langId])
+    //seçilen dil ilk sıraya alınacak şekilde yazdım.
+    ->orderBy('created_at', 'desc')
+    ->get();
 
-        if ($langId && $query !== '') {
-            $results = Content::where('language_id', $langId)
-                ->where(function ($q) use ($query) {
-                    $q->where('title', 'LIKE', "%{$query}%")
-                      ->orWhere('content', 'LIKE', "%{$query}%");
-                })
-                ->orderBy('created_at', 'desc')
-                ->get();
-        }
 
         return view('search', compact('results', 'query', 'languages', 'langId'));
     }
